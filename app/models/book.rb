ed7b8dcc9234310058e819e4de14d68319d2c6f2
EC2 Default User <ec2-user@ip-172-31-18-31.ec2.internal>
2022-03-18 12:12:46 +0000

    [Add]Books作成中

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@ class Book < ApplicationRecord
 
   has_one_attached :profile_image
   belongs_to :user
+  
+  def get_profile_image
+    if profile_image.attached?
+      profile_image
+    else
+      'no_image'
+    end
+  end
 end
